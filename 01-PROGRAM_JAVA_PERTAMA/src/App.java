273774b56d43ffb7a7bdfc619a1ed70880ed13ad
MSyakirArridho <146050299+23241066@users.noreply.github.com>
2023-10-31 13:17:06 +0800

    kuliah_Algo_31_Oktober_2023

---
+++ 01-PROGRAM_JAVA_PERTAMA/src/App.java
@@ -0,0 +1,34 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // Program Biodata Mahasiswa
+        // Deklerasi variable
+        String nama;
+        String nim;
+        String tgl_lahir;
+        String alamat;
+        float berat_badan;
+        float tinggi_badan;
+
+        // mengisi variable
+        nama = "MSyakirArridho";
+        nim = "23241066";
+        tgl_lahir = "20 September 2004";
+        alamat = "tanjung karang";
+        berat_badan = 69.5f;
+        tinggi_badan = 170.5f;
+
+        // cetak Biodata
+        System.out.println("================");
+        System.out.println("Nama : " + nama);
+        System.out.println("NIM : " + nim);
+        System.out.println("Tanggal Lahir : " + tgl_lahir);
+        System.out.println("Alamat : " + alamat);
+        System.out.println("Berat Badan " + berat_badan);
+        System.out.println("Tinggi Badan " + tinggi_badan);
+        System.out.println("================");
+
+
+
+
+    }
+}
